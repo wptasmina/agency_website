1254961a127a58adadd81f031e6bfdff5c6bc419
Tasmina <109110316+wptasmina@users.noreply.github.com>
2024-01-19 20:00:53 +0600

    Add files via upload

--- asseste/Js/script.js
+++ asseste/Js/script.js
@@ -1,8 +1,26 @@
 const faqs = document.querySelectorAll('.faq');
+
+const searchBox = document.querySelector(".search_box");
+const searchBtn = document.querySelector(".search_btn");
+const closeBtn = document.querySelector(".close_btn");
+const search = document.querySelector(".search");
+
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active")
     });
 });
 
+searchBtn.addEventListener('click', function(){
+    
+        search.classList.add("active");
+        searchBox.focus();
+    
+})
 
+closeBtn.addEventListener('click', function() {
+    search.classList.remove("active");
+    searchBox.value = '';
+})
+
+console.log(closeBtn);
